ddaf6f6530671f627b863363ab0915e42200632a
est-anime <134935100+est-anime@users.noreply.github.com>
2024-06-03 19:22:31 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
+// models/User.js
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
