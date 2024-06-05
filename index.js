77f5aba28e08a35793eef74967befc99fa3d22cd
est-anime <134935100+est-anime@users.noreply.github.com>
2024-06-05 16:03:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 const crypto = require('crypto'); // For generating unique filenames
 
-require('dotenv').config(); // Load environment variables from .env file
-
 const app = express();
 const port = process.env.PORT || 3000;
 
