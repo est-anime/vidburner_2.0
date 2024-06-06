86e05be80549d0318cabc38dd57380d613bcd7e6
est-anime <134935100+est-anime@users.noreply.github.com>
2024-06-06 10:02:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/upload', (req, res) => {
   const selectedFont = req.body.font || 'Arial-Bold';
   const outputFileName = req.body.outputFileName || 'output.mp4';
   const userEmail = req.body.email;
-  const watermarkPosition = req.body.watermarkPosition || 'tr';
+  const watermarkPosition = req.body.watermarkPosition || 'main_w-overlay_w-10:10';
 
   const uniqueId = crypto.randomBytes(16).toString('hex');
   const videoPath = path.join(__dirname, `/uploads/video_${uniqueId}.mp4`);
@@ -68,104 +68,113 @@ app.post('/upload', (req, res) => {
             console.error(`Error: ${err.message}`);
             return res.status(500).send('Error occurred while uploading the watermark.');
           }
-        });
 
-        watermarkFilter = `-i "${watermarkPath}" -filter_complex "overlay=${watermarkPosition}"`;
+          watermarkFilter = `-i "${watermarkPath}" -filter_complex "[0:v][1:v] overlay=${watermarkPosition}"`;
+
+          // Proceed with ffmpeg command after the watermark has been uploaded
+          processVideoWithSubtitlesAndWatermark();
+        });
+      } else {
+        processVideoWithSubtitlesAndWatermark();
       }
 
-      const fontMapping = {
-        'Arial-Bold': 'Arial-Bold.ttf',
-        'Juventus Fans Bold': 'Juventus-Fans-Bold.ttf',
-        'Tungsten-Bold': 'Tungsten-Bold.ttf'
-      };
-
-      const selectedFontFile = fontMapping[selectedFont];
-
-      if (!selectedFontFile) {
-        return res.status(400).send('Selected font is not supported.');
-      }
-
-      const fullFontPath = path.join(__dirname, 'fonts', selectedFontFile);
-
-      const subtitlesExtension = path.extname(subtitlesFile.name).toLowerCase();
-      const acceptedSubtitleFormats = ['.srt', '.ass'];
-
-      if (!acceptedSubtitleFormats.includes(subtitlesExtension)) {
-        return res.status(400).send('Selected subtitle format is not supported.');
-      }
-
-      const ffmpegCommand = `ffmpeg -i "${videoPath}" -vf "subtitles=${subtitlesPath}:force_style='FontName=${fullFontPath}'", ${watermarkFilter} "${outputPath}"`;
-
-      const ffmpegProcess = exec(ffmpegCommand);
-
-      let totalFrames = 0;
-      let processedFrames = 0;
-      readline.createInterface({ input: ffmpegProcess.stderr })
-        .on('line', (line) => {
-          if (line.includes('frame=')) {
-            const match = line.match(/frame=\s*(\d+)/);
-            if (match && match[1]) {
-              processedFrames = parseInt(match[1]);
-            }
-          }
-          if (line.includes('fps=')) {
-            const match = line.match(/fps=\s*([\d.]+)/);
-            if (match && match[1]) {
-              totalFrames = parseInt(match[1]);
-            }
-          }
-          if (totalFrames > 0 && processedFrames > 0) {
-            const progressPercent = (processedFrames / totalFrames) * 100;
-            res.write(`data: ${progressPercent}\n\n`);
-          }
-        });
-
-      ffmpegProcess.on('error', (error) => {
-        console.error(`Error: ${error.message}`);
-        return res.status(500).send('Error occurred during video processing.');
-      });
-
-      ffmpegProcess.on('exit', () => {
-        res.write('data: 100\n\n');
-        res.end();
-
-        const downloadLink = `http://${req.hostname}/uploads/${outputFileName}`;
-
-        const transporter = nodemailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: 587,
-          secure: false,
-          auth: {
-            user: process.env.Email,
-            pass: process.env.APP_KEY,
-          },
-        });
-
-        const mailOptions = {
-          from: process.env.Email,
-          to: userEmail,
-          subject: 'Video Encoding Completed',
-          text: `Your video has been successfully encoded. You can download it using the following link: ${downloadLink}`,
+      function processVideoWithSubtitlesAndWatermark() {
+        const fontMapping = {
+          'Arial-Bold': 'Arial-Bold.ttf',
+          'Juventus Fans Bold': 'Juventus-Fans-Bold.ttf',
+          'Tungsten-Bold': 'Tungsten-Bold.ttf'
         };
 
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.error(`Email sending error: ${error}`);
-          } else {
-            console.log(`Email sent: ${info.response}`);
-          }
-        });
+        const selectedFontFile = fontMapping[selectedFont];
 
-        setTimeout(() => {
-          fs.unlink(outputPath, (err) => {
-            if (err) {
-              console.error(`Error deleting file: ${err}`);
-            } else {
-              console.log('Processed video deleted successfully after 24 hours.');
+        if (!selectedFontFile) {
+          return res.status(400).send('Selected font is not supported.');
+        }
+
+        const fullFontPath = path.join(__dirname, 'fonts', selectedFontFile);
+
+        const subtitlesExtension = path.extname(subtitlesFile.name).toLowerCase();
+        const acceptedSubtitleFormats = ['.srt', '.ass'];
+
+        if (!acceptedSubtitleFormats.includes(subtitlesExtension)) {
+          return res.status(400).send('Selected subtitle format is not supported.');
+        }
+
+        const ffmpegCommand = watermarkFilter
+          ? `ffmpeg -i "${videoPath}" ${watermarkFilter} -vf "subtitles=${subtitlesPath}:force_style='FontName=${fullFontPath}'" "${outputPath}"`
+          : `ffmpeg -i "${videoPath}" -vf "subtitles=${subtitlesPath}:force_style='FontName=${fullFontPath}'" "${outputPath}"`;
+
+        const ffmpegProcess = exec(ffmpegCommand);
+
+        let totalFrames = 0;
+        let processedFrames = 0;
+        readline.createInterface({ input: ffmpegProcess.stderr })
+          .on('line', (line) => {
+            if (line.includes('frame=')) {
+              const match = line.match(/frame=\s*(\d+)/);
+              if (match && match[1]) {
+                processedFrames = parseInt(match[1]);
+              }
+            }
+            if (line.includes('fps=')) {
+              const match = line.match(/fps=\s*([\d.]+)/);
+              if (match && match[1]) {
+                totalFrames = parseInt(match[1]);
+              }
+            }
+            if (totalFrames > 0 && processedFrames > 0) {
+              const progressPercent = (processedFrames / totalFrames) * 100;
+              res.write(`data: ${progressPercent}\n\n`);
             }
           });
-        }, 24 * 60 * 60 * 1000);
-      });
+
+        ffmpegProcess.on('error', (error) => {
+          console.error(`Error: ${error.message}`);
+          return res.status(500).send('Error occurred during video processing.');
+        });
+
+        ffmpegProcess.on('exit', () => {
+          res.write('data: 100\n\n');
+          res.end();
+
+          const downloadLink = `http://${req.hostname}/uploads/${outputFileName}`;
+
+          const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+              user: process.env.Email,
+              pass: process.env.APP_KEY,
+            },
+          });
+
+          const mailOptions = {
+            from: process.env.Email,
+            to: userEmail,
+            subject: 'Video Encoding Completed',
+            text: `Your video has been successfully encoded. You can download it using the following link: ${downloadLink}`,
+          };
+
+          transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+              console.error(`Email sending error: ${error}`);
+            } else {
+              console.log(`Email sent: ${info.response}`);
+            }
+          });
+
+          setTimeout(() => {
+            fs.unlink(outputPath, (err) => {
+              if (err) {
+                console.error(`Error deleting file: ${err}`);
+              } else {
+                console.log('Processed video deleted successfully after 24 hours.');
+              }
+            });
+          }, 24 * 60 * 60 * 1000);
+        });
+      }
     });
   });
 });
