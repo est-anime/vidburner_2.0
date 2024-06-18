169ee6146a028c904ce66d254ad895373f66f357
est-anime <134935100+est-anime@users.noreply.github.com>
2024-06-18 21:46:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -114,8 +114,8 @@ app.post('/upload', (req, res) => {
       return res.status(400).send('Selected subtitle format is not supported.');
     }
 
-    const ffmpegCommand = `ffmpeg -i "${videoPath}" -i "${logoPath}" -filter_complex "[1][0]scale2ref=w=iw/5:h=ow/mdar[logo][video];[video][logo]overlay=W-w-10:10,subtitles=${subtitlesPath}:fontfile='${fullFontPath}'" "${outputPath}"`;
-
+    const ffmpegCommand = `ffmpeg -i "${videoPath}" -i "${logoPath}" -filter_complex "[1][0]scale2ref=w=iw/5:h=ow/mdar[logo][video];[video][logo]overlay=W-w-10:10,subtitles=${subtitlesPath}:force_style='FontName=${fullFontPath}'" "${outputPath}"`;
+ 
     executeFfmpeg(ffmpegCommand);
   };
 
@@ -141,7 +141,8 @@ app.post('/upload', (req, res) => {
       return res.status(400).send('Selected subtitle format is not supported.');
     }
 
-    const ffmpegCommand = `ffmpeg -i "${videoPath}" -vf "subtitles=${subtitlesPath}:fontfile='${fullFontPath}'" "${outputPath}"`;
+    const ffmpegCommand = `ffmpeg -i "${videoPath}" -vf "subtitles=${subtitlesPath}:force_style='FontName=${fullFontPath}'" "${outputPath}"`;
+    
     executeFfmpeg(ffmpegCommand);
   };
 
