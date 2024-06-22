bf41b766ed1d917c3d442adc025b18a5c43a37ba
est-anime <134935100+est-anime@users.noreply.github.com>
2024-06-22 10:53:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { exec } = require('child_process');
 const fs = require('fs');
 const readline = require('readline');
 const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 const path = require('path');
 const crypto = require('crypto'); // For generating unique filename
 
