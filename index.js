c8c484b378861ffd62ef1d70753f1f51c165ac6c
est-anime <134935100+est-anime@users.noreply.github.com>
2024-06-22 10:42:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,22 +14,6 @@ app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-// MySQL Database Connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-  console.log('Connected to database');
-});
-
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
