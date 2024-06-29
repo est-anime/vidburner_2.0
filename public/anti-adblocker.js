6f83305106f4ad29709548be319d9491f637e491
est-anime <134935100+est-anime@users.noreply.github.com>
2024-06-29 18:41:43 +0530

    Update anti-adblocker.js

--- public/anti-adblocker.js
+++ public/anti-adblocker.js
@@ -1,19 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Create a bait element
-    var adElement = document.createElement('div');
-    adElement.className = 'adsbox';
-    adElement.style.display = 'none';
-    document.body.appendChild(adElement);
-
-    // Check if the ad element is blocked
+document.addEventListener('DOMContentLoaded', function() {
+    // Check if an adblocker is enabled
     setTimeout(function() {
-        if (!adElement || adElement.offsetParent === null || adElement.offsetHeight === 0) {
-            // Adblocker is enabled
-            document.getElementById('adblocker-popup').style.display = 'flex';
-            document.body.classList.add('blur');
-        } else {
-            // Adblocker is not enabled
-            adElement.style.display = 'none';
-        }
+        var adsElement = document.createElement('div');
+        adsElement.className = 'adsbox';
+        adsElement.style.display = 'none';
+        document.body.appendChild(adsElement);
+
+        // Check if the ads element is blocked
+        setTimeout(function() {
+            if (!adsElement || adsElement.offsetParent === null || adsElement.offsetHeight === 0) {
+                // Adblocker is enabled, show the adblocker popup
+                var adblockerPopup = document.getElementById('adblocker-popup');
+                if (adblockerPopup) {
+                    adblockerPopup.style.display = 'flex';
+                    document.body.classList.add('blur');
+                }
+            }
+        }, 100);
     }, 100);
 });
